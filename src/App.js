34d3f3f552ab919e689daebffe48b7b451c6cf95
Bianca L. Hall <biancarose1211@gmail.com>
2023-01-18 15:57:17 +0100

    Adjust text again

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
       >
         Generate
       </button>
-      <div>{`Generated color: ${colorChip}}`}</div>
+      <div>{`Generated color: ${colorChip}`}</div>
     </div>
   );
 }
