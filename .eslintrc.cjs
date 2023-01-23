169c2fa1fbbf67764725690fe057f33660869105
Bianca L. Hall <biancarose1211@gmail.com>
2023-01-23 14:41:43 +0100

    Removed package json update Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,5 @@
 const config = {
   extends: ['@upleveled/upleveled'],
 };
-module.exports = {
-  rules: {
-    // Note: you must disable the base rule as it can report incorrect errors
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-  },
-};
+
 module.exports = config;
